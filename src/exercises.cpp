d453d5f9eb64e26878cec2016cbf2869dfbc2a48
W3wallace <159574422+W3wallace@users.noreply.github.com>
2024-05-02 21:39:13 -0400

    23

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,69 +5,66 @@ using namespace std;
 
 void exercise_1(string s1) {
     for(int i = 0; i <= s1.size(); ++i){
-        if (s1[i] == ' '){
-          cout << i << endl;
+      if (s1[i] == ' '){
+        cout << i << endl;
       }
     }
     cout << s1.size() << endl;
-  
 }
 
 void exercise_2(string s1) {
-  
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_3(string s1) {
-  
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_4(int n) {
- 
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_5(int n, int k) {
-  
+  // TODO: YOUR CODE HERE
 }
 
 int exercise_6(int n) {
-
-    
+  // TODO: YOUR CODE HERE
   return 0;
 }
 
 void exercise_7(int n) {
- 
-  return 0;
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_8(string s) {
-  
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_9(string s) {
-  
+  // TODO: YOUR CODE HERE
 }
 
 int exercise_10(int a, int b) {
-  
-  return sus;
+  // TODO: YOUR CODE HERE
+  return 0;
 }
 
 void exercise_11() {
-  
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_12() {
- 
+  // TODO: YOUR CODE HERE
 }
 
 long exercise_13(int n, int k) {
-   
+  // TODO: YOUR CODE HERE
   return 0;
 }
 
 string exercise_14(int n) {
-  
+  // TODO: YOUR CODE HERE
   return "";
 }
 
@@ -76,11 +73,11 @@ void exercise_15(int decimal) {
 }
 
 void exercise_16(int divident, int divider) {
- 
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_17(int n) {
- 
+  // TODO: YOUR CODE HERE
 }
 
 void exercise_18_19(int debut, int fin) {
