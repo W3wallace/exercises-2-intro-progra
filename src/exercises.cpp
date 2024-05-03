26251abc1c62d19fbd20f1a4e1e390bdbaecc715
W3wallace <159574422+W3wallace@users.noreply.github.com>
2024-05-02 23:51:10 -0400

    34

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -25,6 +25,65 @@ void exercise_4(int n) {
 }
 
 void exercise_5(int n, int k) {
+  string espacio1;
+    switch(n){
+        case 1:
+        espacio1 =  " "s;
+        break;
+        case 2:
+        espacio1 =  "  "s;
+        break;
+        case 3:
+        espacio1 =  "     "s;
+        break;
+        case 4:
+        espacio1 =  "        "s;
+        break;
+        case 5:
+        espacio1 =  "           "s;
+        break;
+        case 6:
+        espacio1 =  "              "s;
+        break;
+        case 7:
+        espacio1 =  "                 "s;
+        break;
+    }
+
+    string _i;
+    for(int i = 1; i<=k; ++i){
+        int p = 20;
+        int r = 0;
+        while(r <= 14){
+            if(espacio1.size() == p + (p*r) + r){
+              espacio1= espacio1 + '\n';
+              break;
+            }
+            ++r;
+        }
+        if(n==1 && espacio1.size()==1){
+          espacio1 = espacio1 + to_string(i);
+        }
+        else if(to_string(i).size()==1 && espacio1[espacio1.size()-1]!='\n'){
+            _i = "  " + to_string(i);
+            espacio1 = espacio1 + _i;
+        }
+        else if(to_string(i).size()==1 && espacio1[espacio1.size()-1]=='\n'){
+            _i = " " + to_string(i);
+            espacio1 = espacio1 + _i;
+
+        }
+        else if(to_string(i).size()==2 && espacio1[espacio1.size()-1]!='\n'){
+            _i = " " + to_string(i);
+            espacio1 = espacio1 + _i;
+        }
+        else if(to_string(i).size()==2 && espacio1[espacio1.size()-1]=='\n'){
+            espacio1 = espacio1 + to_string(i);
+        }
+
+    }
+    cout << espacio1 << " " << endl;
+}
 
 }
 
@@ -52,7 +111,7 @@ void exercise_9(string s) {
   }
   switch(cantidad){
     case 0:
-    cout <<-2 << endl;
+    cout << -2 << endl;
     break;
     case 1:
     cout << -1 << endl;
